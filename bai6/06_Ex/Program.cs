87c855e428ae8ced97d26db2136ed728089fd0e3
mtv1001 <71858427+mtv1001@users.noreply.github.com>
2020-10-12 21:04:27 +0700

    bai6

---
+++ bai6/06_Ex/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_Exercise
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Student> stu = new List<Student>();
+            int select;
+            do
+            {
+                Console.WriteLine("1. Add Student\t2. Exit\nYour select: ");
+                select = int.Parse(Console.ReadLine());
+                if (select == 1)
+                {
+                    Student a = new Student();
+                    a.Input();
+                    stu.Add(a);
+                }
+            } while (select != 2);
+
+            Console.WriteLine("\n================Student List =====================");
+            //stu.Sort();
+            stu.Sort(new NameComparer());
+            foreach (Student x in stu)
+            {
+                x.Output();
+            }
+        }
+    }
+}
---
+++ bai6/06_Ex/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _06_Exercise
+{
+    class Student : IComparable<Student>
+    {
+        public string Name;
+        public string StudentID;
+
+        public Student()
+        {
+
+        }
+        public string name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
+
+        public string ID
+        {
+            get
+            {
+                return StudentID;
+            }
+            set
+            {
+                StudentID = value;
+            }
+        }
+
+        public void Input()
+        {
+            Console.Write("Input student's name: ");
+            Name = Console.ReadLine();
+            Console.Write("Input Student's ID: ");
+            StudentID = Console.ReadLine();
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"{Name} - {StudentID}");
+        }
+        public int CompareTo(Student other)
+        {
+            return this.StudentID.CompareTo(other.StudentID);
+        }
+        public int CompareTo1(Student other)
+        {
+            return this.Name.CompareTo(other.Name);
+        }
+    }
+
+    class NameComparer : IComparer<Student>
+    {
+        public int Compare(Student x, Student y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+    }
+}
